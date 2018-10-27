bf588b348de33ba443e8b6ff8ab4c990120a6e22
Akshar Raaj <akshar@agiliq.com>
2018-10-27 23:53:19 +0530

    Make form look better

--- trades/forms.py
+++ trades/forms.py
@@ -6,3 +6,8 @@ class AlgoForm(forms.Form):
     signal = forms.CharField(max_length=1000)
     trade = forms.CharField(max_length=500)
     ticker = forms.CharField(max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super(AlgoForm, self).__init__(*args, **kwargs)
+        for field in self.fields.keys():
+            self.fields[field].widget.attrs.update({'class': 'form-control '})
