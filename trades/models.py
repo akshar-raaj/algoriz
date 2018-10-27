d858799d0adbfacb3a5ae576d09a5d7eaaff88b4
Akshar Raaj <akshar@agiliq.com>
2018-10-28 00:10:52 +0530

    Added model

--- trades/models.py
+++ trades/models.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Algorithm(models.Model):
+    name = models.CharField(max_length=100)
+    positions = ArrayField(models.TextField(), null=True, blank=True, default=list)
+    PnL = ArrayField(models.TextField(), null=True, blank=True, default=list)
+
+    def __unicode__(self):
+        return self.name
