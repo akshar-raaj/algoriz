9fdaacd6ebe4f7bfe88e9b4ccce3b2529e711e10
Akshar Raaj <akshar@agiliq.com>
2018-10-27 23:45:31 +0530

    Render form on page

--- trades/views.py
+++ trades/views.py
@@ -3,12 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+
+from .forms import AlgoForm
 
 
 class CreateAlgoView(View):
     template_name = 'trades/create-algo.html'
     def get(self, request, *args, **kwargs):
+        algo_form = AlgoForm()
         context = {
+            'algo_form': algo_form
         }
         return render(request, self.template_name, context)
